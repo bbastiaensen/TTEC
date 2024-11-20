7ad6e0134989da5559d9103540cd0e9d4be65dec
liam <liamkelleher2006@gmail.com>
2024-11-20 21:50:58 +0100

    fix  navbar mobile + test icon

--- TTEC/js/logic/JavaScript.js
+++ TTEC/js/logic/JavaScript.js
@@ -1,1 +1,11 @@
-﻿
+﻿$(document).ready(function () {
+    $("#blur").hide();
+    $("#test2").click(function () {
+        $("#blur").fadeIn();
+        $("#mySidenav").css("width", "45%");
+    });
+    $("#test, #blur").click(function () {
+        $("#blur").fadeOut();
+        $("#mySidenav").css("width", "0");
+    });
+});

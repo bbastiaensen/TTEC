57cd7ad26974531886d365fc658136161130d929
Jesse Meulenbroek <Meulenbroekjesse250@gmail.com>
2024-11-26 15:13:51 +0100

    Code overzetten

---
+++ TTECLogic/Object/Registratie.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TTECLogic.Object
+{
+    public class Registratie
+    {
+        public string Gebruikersnaam { get; set; }
+
+        public string Voornaam { get; set; }
+
+        public string Achternaam { get; set; }
+
+        public bool CampusZenit { get; set; }
+
+        public bool CampusBoomgaard { get; set; }
+
+    }
+}

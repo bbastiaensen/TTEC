fba57656f6089e06843a26f2a0016a93c280bbaa
liamwibo <liamkelleher2006@gmail.com>
2024-11-26 15:33:50 +0100

    aanmaak login.aspx

--- TTEC/Site.Master.designer.cs
+++ TTEC/Site.Master.designer.cs
@@ -24,6 +24,15 @@ namespace TTEC
         protected global::System.Web.UI.WebControls.ContentPlaceHolder head;
 
         /// <summary>
+        /// form1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+
+        /// <summary>
         /// nav control.
         /// </summary>
         /// <remarks>
@@ -31,6 +40,24 @@ namespace TTEC
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.ContentPlaceHolder nav;
+
+        /// <summary>
+        /// Button1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Button1;
+
+        /// <summary>
+        /// Button2 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Button2;
 
         /// <summary>
         /// ContentPlaceHolder2 control.
